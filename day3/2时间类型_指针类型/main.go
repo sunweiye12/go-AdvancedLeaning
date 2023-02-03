054986718a2403fcc318897b40ed4b178b0850b8
sunweiye.3 <sunweiye.3@bytedance.com>
2023-02-03 16:29:43 +0800

    test

--- day3/2时间类型_指针类型/main.go
+++ day3/2时间类型_指针类型/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+
 func main() {
-	$END$
+	const Layout = "2006-01-02 15:00:00"
+
+	now := time.Now().Unix()+3600*4
+	tm := time.Unix(now, 0)
+	//hourEndTime, _ := time.Parse("2006-01-02 15:00:00", tm.Format("2006-01-02 15:00:00"))
+	hourEndTime, _ := time.Parse(Layout, tm.Format(Layout))
+
+	fmt.Println(now)
+	fmt.Println(tm)
+	fmt.Println(hourEndTime)
+	fmt.Println("test")
+
 }
